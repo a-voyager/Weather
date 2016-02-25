dbf5cfefa3f1907dd83c2c42752a2c41b6701d9e
wuhaojie <w19961009@126.com>
2016-02-25 14:47:32 +0800

    add scroll view.

--- app/src/main/java/com/voyager/weather/MainActivity.java
+++ app/src/main/java/com/voyager/weather/MainActivity.java
@@ -1,20 +1,44 @@
 package com.voyager.weather;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.thinkland.sdk.android.JuheSDKInitializer;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar_main);
-        setSupportActionBar(toolBar);
-        JuheSDKInitializer.initialize(this);
+        initView();
+        initData();
     }
 
+    /**
+     * init data.
+     */
+    private void initData() {
+        JuheSDKInitializer.initialize(getApplicationContext());
+    }
+
+    /**
+     * init view.
+     */
+    private void initView() {
+        Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolBar);
+        SwipeRefreshLayout pull_refresh_view = (SwipeRefreshLayout) findViewById(R.id.pull_refresh_view);
+        pull_refresh_view.setOnRefreshListener(this);
+    }
+
+    /**
+     * call this when user pull to refresh
+     */
+    @Override
+    public void onRefresh() {
+
+    }
 }
